0dc4d495b33018354970bcf6dfbd6ec7265d9787
ray <rui.song90@gmail.com>
2025-01-14 18:31:53 +0000

    update

--- PM25-ML/prediction/step-1-data-preparation.py
+++ PM25-ML/prediction/step-1-data-preparation.py
@@ -5,4 +5,28 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        14/01/2025 17:53
 
-sentinel_data = './S2_London_2018'
+from osgeo import gdal
+
+sentinel_data = '/gws/pw/j07/nceo_aerosolfire/rsong/project/UKIF/PM25-ML/data_preprocess/S2L1C_London/Sentinel2_L1C_20180807_CloudMasked.tif'
+pm25_data =     '/gws/pw/j07/nceo_aerosolfire/rsong/project/UKIF/PM25-ML/data_preprocess/modis_pm25_data_reproject_crop/GHAP_PM2.5_D1K_20180807_V1_cropped_projected.nc'
+
+sentinel_dataset = gdal.Open(sentinel_data)
+target_lat = 51.518 # top left corner of the selected box
+target_lon = -0.136 # top left corner of the selected box
+
+# Get the geotransform and raster size
+geotransform = sentinel_dataset.GetGeoTransform()
+origin_x, pixel_width, _, origin_y, _, pixel_height = geotransform
+raster_x_size = sentinel_dataset.RasterXSize
+raster_y_size = sentinel_dataset.RasterYSize
+
+# Calculate pixel coordinates for the given latitude and longitude
+pixel_x = int((target_lon - origin_x) / pixel_width)
+pixel_y = int((target_lat - origin_y) / pixel_height)
+
+# Ensure the coordinates are within the raster bounds
+pixel_x = max(0, min(raster_x_size - 1, pixel_x))
+pixel_y = max(0, min(raster_y_size - 1, pixel_y))
+
+print(f"Closest pixel index to lat/lon ({target_lat}, {target_lon}):")
+print(f"X: {pixel_x}, Y: {pixel_y}")
