b60658d7c4e58144aa8212e196f0a35286d65588
ray <rui.song90@gmail.com>
2025-01-15 17:20:57 +0000

    update

---
+++ PM25-ML/prediction/step-2-data-predict-patch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    step-2-data-predict-patch.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        15/01/2025 17:03
+
+import tensorflow as tf
+import numpy as np
+import os
+
+# Load the trained model
+model = tf.keras.models.load_model('./pm25_model_pre_v0.h5')
+
+# Define the data folder for testing data
+test_data_folder = './prediction/data_patch'
+
+# Get list of .npz files
+npz_files = [os.path.join(data_folder, f) for f in os.listdir(data_folder) if f.endswith('.npz')]
+print(f"Total .npz files: {len(npz_files)}")
