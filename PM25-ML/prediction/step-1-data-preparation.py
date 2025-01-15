4c1e2803ab97f01c518e32c8284a577e5cdc77b4
ray <rui.song90@gmail.com>
2025-01-15 00:56:25 +0000

    update

--- PM25-ML/prediction/step-1-data-preparation.py
+++ PM25-ML/prediction/step-1-data-preparation.py
@@ -150,8 +150,9 @@ for i in range(crop_x, crop_x_end):
     for j in range(crop_y, crop_y_end):
         # Extract the patch centered at the current pixel
         patch = extract_patch(sentinel_data, i, j)
+
+        # Save the patch to a file with i and j in 3 digits
+        patch_file = os.path.join(save_patch_data_dir, f'patch_{i:03d}_{j:03d}.npy')
         print(patch.shape)
         quit()
-        # Save the patch to a file
-        patch_file = os.path.join(save_patch_data_dir, f'patch_{i}_{j}.npy')
         np.save(patch_file, patch)
