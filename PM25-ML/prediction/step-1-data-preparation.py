7e1fafc859e59e3fa051b0a46a4070c7f1f43e40
ray <rui.song90@gmail.com>
2025-01-15 00:56:50 +0000

    update

--- PM25-ML/prediction/step-1-data-preparation.py
+++ PM25-ML/prediction/step-1-data-preparation.py
@@ -153,6 +153,5 @@ for i in range(crop_x, crop_x_end):
 
         # Save the patch to a file with i and j in 3 digits
         patch_file = os.path.join(save_patch_data_dir, f'patch_{i:03d}_{j:03d}.npy')
-        print(patch.shape)
-        quit()
         np.save(patch_file, patch)
+        quit()
